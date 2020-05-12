3799eb8aebd04a2157512a0b9e88582106bf13eb
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 22:02:22 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,6 +1,13 @@
 def find_min_value(array)
   count = 0
-  min_value =
+  min_value = 2
+  while count < array.length do
+    if min_value > array[count]
+      min_value = array[count]
+    end
+    count += 1
+  end
+  min_value
 end
 
 
