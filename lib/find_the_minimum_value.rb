fdb0b14eae4a1f1079e2d2bcc568dd521e6e378c
Marshall Raiskin <marshall.raiskin@gmail.com>
2020-05-12 22:17:50 +0000

    Automatically backed up by Learn

--- lib/find_the_minimum_value.rb
+++ lib/find_the_minimum_value.rb
@@ -1,9 +1,9 @@
-def find_min_value(scale)
+def find_min_value(array)
   count = 0
   min_value = 1
-  while count > scale.length do
-    if min_value > scale[count]
-      min_value = scale[count]
+  while count > array.length do
+    if min_value > array[count]
+      min_value = array[count]
     end
     count += 1
   end
